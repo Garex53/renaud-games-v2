0fa018bf4c5092ed459e6685fae9e4d19b2d8c7a
𝓑𝓪𝓭𝓢𝓲𝓭𝓮 𝓕𝓐 <158852477+Garex53@users.noreply.github.com>
2026-03-25 02:54:30 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.RG_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAWisHpoel0tBSSmBnpMQc1WNHYFWM9iHI",
+  authDomain: "renaud-game.firebaseapp.com",
+  projectId: "renaud-game",
+  storageBucket: "renaud-game.firebasestorage.app",
+  messagingSenderId: "577594129366",
+  appId: "1:577594129366:web:d2aac426cd82ec45bc8277"
+};
